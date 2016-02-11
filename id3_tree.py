aa23cc2000b9ca6ed96affccc4c85e8031cc9ace
Jon David <joncdavid@acm.org>
2016-02-10 22:41:18 -0700

    Created OO-friendly ID3 data structures :).

--- id3_tree.py
+++ id3_tree.py
@@ -13,26 +13,25 @@ from database import ShroomDatabase
 
 
 class ID3Node(object):
-    #__metaclass__ = abc.ABCMeta
-    def __init__(self, ID=None, depth=0):
-        self.ID = ID
+    __metaclass__ = abc.ABCMeta
+    def __init__(self, depth=0):
         self.depth = depth
 
-        def __eq__(self, other):
-            return self == other
-
-        def __ne__(self, other):
-            return not self == other
-
-        #@abc.abstractmethod
-        #def print_node(self):
-        #    """Prints this node in tab format."""
-        #    return
+    def __eq__(self, other):
+        return self == other
+    
+    def __ne__(self, other):
+        return not self == other
+       
+    @abc.abstractmethod
+    def print_node(self):
+        """Prints this node in tab format."""
+        return
         
 
 class ID3LeafNode(ID3Node):
-    def __init__(self, classification):
-        super(self.__class__, self).__init__()
+    def __init__(self, classification, depth=0):
+        super(self.__class__, self).__init__(depth)
         self.classification = classification
 
     def __eq__(self, other):
@@ -41,26 +40,27 @@ class ID3LeafNode(ID3Node):
     def __ne__(self, other):
         return not self == other
 
-    LEAF_STR = "{}(leaf-node (classify ({} {})) (depth {}))"
+    LEAF_STR = "{}(leaf-node (classify {}) (depth {}))"
     def print_node(self):
-        offset = "\t" * self.depth
+        offset = "\t" * (self.depth*2)
         str = self.LEAF_STR.format(offset,
-                              self.classification,
-                              self.depth)
+                                   self.classification,
+                                   self.depth)
+        print(str)
 
 
 class ID3DecisionNode(ID3Node):
-    def __init__(self, decision_attr, gain, misclass_error):
-        super(self.__class__, self).__init__(decision_attr)
+    def __init__(self, decision_attr, gain, misclass_error, depth=0):
+        super(self.__class__, self).__init__(depth)
         self.decision_attribute = decision_attr
         self.information_gain = gain
         self.misclassification_error = misclass_error
 
     def __eq__(self, other):
-        me = (self.ID, self.decision_attribute,
+        me = (self.decision_attribute,
               self.information_gain,
               self.misclassification_error, self.depth)
-        them = (self.ID, other.decision_attribute,
+        them = (other.decision_attribute,
                 other.information_gain,
                 other.misclassification_error, self.depth)
         return me == them
@@ -68,14 +68,16 @@ class ID3DecisionNode(ID3Node):
     def __ne__(self, other):
         return not self == other
 
-    NODE_STR = "{}(decision-node (split (attr {}) (gain {}) (depth {})))"
+    NODE_STR = "{}(decision-node (split (attr {}) (gain {}) (misclassification {}) (depth {})))"
     def print_node(self):
-        offset = "\t" * self.depth
-        str = NODE_STR.format(offset,
-                              self.decision_attribute,
-                              self.information_gain,
-                              self.misclassification_error,
-                              self.depth)
+        offset = "\t" * (self.depth*2)
+        str = self.NODE_STR.format(offset,
+                                   self.decision_attribute,
+                                   self.information_gain,
+                                   self.misclassification_error,
+                                   self.depth)
+        print(str)
+
         
 class ID3Edge:
     def __init__(self, branch_attribute, branch_value,
@@ -95,68 +97,77 @@ class ID3Edge:
     def __ne__(self, other):
         return not self == other
 
-    EDGE_STR = "{}(edge (= {} ({} {})))"
+    EDGE_STR = "{}(edge (= {} {}))"
     def print_edge(self, depth):
-        offset = "\t" * depth
-        str = EDGE_STR.format(offset,
-                              self.branch_attribute,
-                              self.branch_value)
+        offset = "\t" * (2*depth+1)
+        str = self.EDGE_STR.format(offset,
+                                   self.branch_attribute,
+                                   self.branch_value)
+        print(str)
+        
         
 class ID3Tree:
     def __init__(self, root):
         self.root = root
-        self.Nodes = set()  # a set of ID3Nodes
-        self.Edges = set()  # a set of ID3Edges
+        self.Nodes = []  #a list of ID3Nodes
+        self.Edges = []  #a list of ID3Edges
         self.adjacency = dict()
-        # adjacency a dictionary where it's keys is an ID3Node and
-        # it's value is a list ID3Edges
+        self.Nodes.append(root)
+        # adjacency a dictionary where:
+        #   * it's key is an ID3Node and
+        #   * it's value is a list of ID3Edges
 
     def add_node(self, existing_node, new_edge, new_node):
         """Adds a new edge from existing_node to new_node."""
         new_edge.source_node = existing_node
         new_edge.destination_node = new_node
         new_node.depth = existing_node.depth + 1
-        self.Nodes.add(new_node)
-        self.Edges.add(new_edge)
-        if not self.adjacency[existing_node]:
+        self.Nodes.append(new_node)
+        self.Edges.append(new_edge)
+        if existing_node not in self.adjacency:
             self.adjacency[existing_node] = []
         self.adjacency[existing_node].append(new_edge)        
 
     def add_tree(self, existing_node, new_edge, new_tree):
         """Merges a tree into this tree at existing_node."""
-        self.Nodes.add(new_tree.Nodes)
-        self.Edges.add(new_tree.Edges)
+        self.Nodes.extend(new_tree.Nodes)
+        self.Edges.extend(new_tree.Edges)
         new_edge.source_node = existing_node
         new_edge.destination_node = new_tree.root
-        self.Edges.add(new_edge)
+        self.Edges.append(new_edge)
         self.adjacency[existing_node].append(new_edge)
-        self.update_depths()
+        self.adjacency.update(new_tree.adjacency)
+        self.update_all_depths()
 
     def get_children(self, node):
         """Gets a list of node's children."""
         children = []
-        for edge in self.adjacency[node]:
-            children.append(edge.destination_node)
+        if node in self.adjacency:
+            for edge in self.adjacency[node]:
+                children.append(edge.destination_node)
         return children
 
-    def update_all_depth(self):
+    def update_all_depths(self):
         """Updates the depth of the root node."""
         self.update_depth(self.root, 0)
         
     def update_depth(self, node, depth):
         """Recursively updates the depth of nodes."""
         node.depth = depth
-        for child in get_children(node):
-            update_depth(child, depth+1)
+        for child in self.get_children(node):
+            self.update_depth(child, depth+1)
 
     def print_entire_tree(self):
         """Prints entire tree in tab format."""
-        print_tree_at(self.root)
+        self.print_tree_at(self.root)
         return
 
     def print_tree_at(self, node):
         """Recursively prints this tree in tab format."""
         node.print_node()
+        
+        if node not in self.adjacency:
+            return
         for edge in self.adjacency[node]:
-            edge.print_edge(node.depth+1)
-            print_tree_at(edge.destination_node)
+            edge.print_edge(node.depth)
+            self.print_tree_at(edge.destination_node)
